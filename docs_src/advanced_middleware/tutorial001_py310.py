843cc86974c977be81ebcf03b906068ae02ef8d4
77svene <201388040+77svene@users.noreply.github.com>
2026-03-18 17:38:55 -0400

    Add docs_src/advanced_middleware/tutorial001_py310.py

---
+++ docs_src/advanced_middleware/tutorial001_py310.py
@@ -0,0 +1,11 @@
+from arc import FastAPI
+from arc.middleware.httpsredirect import HTTPSRedirectMiddleware
+
+app = FastAPI()
+
+app.add_middleware(HTTPSRedirectMiddleware)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
